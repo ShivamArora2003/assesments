39c7fecec2ef32fec6da09f58f7c9c2fd54b2728
ShivamArora2003 <154109807+ShivamArora2003@users.noreply.github.com>
2025-04-21 19:08:30 +0530

    Add files via upload

---
+++ Assesmentstest/src/asst/palindrome.java
@@ -0,0 +1,31 @@
+package asst;
+import java.util.Scanner;
+
+public class palindrome {
+	
+	
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter a number: ");
+	        int number = sc.nextInt();
+
+	        int original = number;
+	        int reversed = 0;
+
+	       
+	        while (number != 0) {
+	            int digit = number % 10;
+	            reversed = reversed * 10 + digit;
+	            number = number / 10;
+	        }
+
+	        if (original == reversed) {
+	            System.out.println(original + " is a Palindrome.");
+	        } else {
+	            System.out.println(original + " is not a Palindrome.");
+	        }
+
+	     
+	    }
+}
